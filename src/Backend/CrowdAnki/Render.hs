eaa753ac1b8f369f14c1173be23ec0c36b4b3d50
Vittorio Zaccaria <vittorio.zaccaria@gmail.com>
2018-11-01 15:08:44 +0100

    update: produce latex compatible titles (Render.hs, Parse.hs)

--- src/Backend/CrowdAnki/Render.hs
+++ src/Backend/CrowdAnki/Render.hs
@@ -1,62 +1,71 @@
-{-# LANGUAGE FlexibleContexts  #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE QuasiQuotes       #-}
+{-# LANGUAGE QuasiQuotes #-}
 
 module Backend.CrowdAnki.Render where
 
-import           Data.Aeson.Encode.Pretty 
+import Data.Aeson.Encode.Pretty
 import qualified Data.ByteString.Lazy.Char8 as BL
-import           Backend.CrowdAnki.Deck
-import           Backend.CrowdAnki.Note
-import           Backend.CrowdAnki.NoteModel
-import           Internal.Parse
-import           Data.List
-import           Data.List.Split
-import qualified Data.Map                    as Map
-import           Data.Maybe
-import           Data.String.Interpolate
-import           Data.Tree
-import           Text.Pandoc
-import           Text.Pandoc.Walk            (walk)
-import           Utils
+import Backend.CrowdAnki.Deck
+import Backend.CrowdAnki.Note
+import Backend.CrowdAnki.NoteModel
+import Internal.Parse
+import Data.List
+import Data.List.Split
+import qualified Data.Map as Map
+import Data.Maybe
+import Data.String.Interpolate
+import Data.Tree
+import Text.Pandoc
+import Text.Pandoc.Walk (walk)
+import Utils
 
-processLeafContentsAsLatex :: [Block] -> String
-processLeafContentsAsLatex b = 
-  let ltx = writeLaTeX def (Pandoc (Meta Map.empty) b) 
-   in "[latex]" ++ ltx ++"[/latex]"
+inlineToLatex :: [Inline] -> String
+inlineToLatex i = blockToLatex $ [Plain i]
 
-renderLeafAsLatex :: InternalDeckStructLeaf -> String
-renderLeafAsLatex l = processLeafContentsAsLatex . contents $ l
+blockToLatex :: [Block] -> String
+blockToLatex b =
+    let ltx = writeLaTeX def (Pandoc (Meta Map.empty) b)
+    in "[latex]" ++ ltx ++ "[/latex]"
 
-renderStructIntoNote :: InternalDeckStruct -> Maybe Note   
-renderStructIntoNote (Node q _) =
-  let noteTitle = [i|[latex]#{getName q}[/latex]|]
-      noteContent = renderLeafAsLatex q
-      note = N [noteTitle, noteContent] 0 (def :: NoteModel) [] ""
-      opt = Map.lookup "noanki" (getHeadingMeta q)
-  in case opt of 
-       Just _  -> Nothing
-       Nothing -> Just note
+getNodeConcept (Node q _) = q
 
-renderStructure :: Integer -> Integer -> InternalDeck -> InternalDeckStruct -> Deck
-renderStructure curlev cardlev idk s =
-  if curlev == (cardlev - 1)
-    then let (Node concept childs) = s
-             name = getName concept
-             notes = mapMaybe renderStructIntoNote childs
-         in (def :: Deck) {d_notes = notes, d_name = name}
-    else let (Node concept childs) = s
-             name =
-               if (curlev == 0)
-                 then getTitle idk
-                 else getName concept
-             decks = map (renderStructure (curlev + 1) cardlev idk) childs
-         in (def :: Deck) {d_children = decks, d_name = name}
+renderConceptToLatexNote :: Concept -> Maybe Note
+renderConceptToLatexNote q =
+    let noteTitle = inlineToLatex . getTitle $ q
+        noteContent = blockToLatex . getConceptContents $ q
+        note = N [noteTitle, noteContent] 0 (def :: NoteModel) [] ""
+        opt = Map.lookup "noanki" (getHeadingMeta q)
+    in case opt of
+           Just _ -> Nothing
+           Nothing -> Just note
 
 internalDeckToDeck :: InternalDeck -> Deck
-internalDeckToDeck d = renderStructure 0 (getCardLevel d) d $ getInternalDeckStruct d
+internalDeckToDeck idk =
+    let renderConceptTree curlev cardlev s =
+            if curlev == (cardlev - 1)
+                then let (Node concept trees) = s
+                         subdeckname = getName concept
+                         notes =
+                             mapMaybe
+                                 (renderConceptToLatexNote . getNodeConcept)
+                                 trees
+                     in (def :: Deck)
+                        { d_notes = notes
+                        , d_name = subdeckname
+                        }
+                else let (Node concept trees) = s
+                         name =
+                             if (curlev == 0)
+                                 then getDeckName idk
+                                 else getName concept
+                         decks =
+                             map (renderConceptTree (curlev + 1) cardlev) trees
+                     in (def :: Deck)
+                        { d_children = decks
+                        , d_name = name
+                        }
+    in renderConceptTree 0 (getCardLevel idk) $ getConceptTree idk
 
 renderAsCrowdAnki :: InternalDeck -> IO String
 renderAsCrowdAnki d = d_dumpStringIO $ internalDeckToDeck d
-
-
--- src/Internal/Parse.hs
+++ src/Internal/Parse.hs
@@ -1,89 +1,87 @@
-{-# LANGUAGE DeriveAnyClass             #-}
-{-# LANGUAGE DeriveGeneric              #-}
-{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE OverloadedStrings          #-}
-{-# LANGUAGE QuasiQuotes                #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
 
 module Internal.Parse where
 
-import           Data.Aeson
-import           Data.Aeson.Encode.Pretty
+import Data.Aeson
+import Data.Aeson.Encode.Pretty
 import qualified Data.ByteString.Lazy.Char8 as BL
-import           Data.Char                  (toLower)
-import           Data.List
-import           Data.List.Split
-import qualified Data.Map                   as Map
-import           Data.Maybe
-import           Data.String.Interpolate
-import qualified Data.Text                  as T
-import           Data.Tree
-import           Debug.Trace
-import           GHC.Generics
-import           System.Directory
-import           System.Process
-import           Text.Pandoc
-import           Text.Pandoc.Error
-import           Text.Pandoc.Walk           (walk)
-import           Utils
+import Data.Char (toLower)
+import Data.List
+import Data.List.Split
+import qualified Data.Map as Map
+import Data.Maybe
+import Data.String.Interpolate
+import qualified Data.Text as T
+import Data.Tree
+import Debug.Trace
+import GHC.Generics
+import System.Directory
+import System.Process
+import Text.Pandoc
+import Text.Pandoc.Error
+import Text.Pandoc.Walk (walk)
+import Utils
 
 type HeadingMeta = Map.Map String String
 
-data InternalDeckStructLeaf = Concept
-  { getID          :: String
-  , getUID         :: Integer
-  , getName        :: String
-  , getHeadingMeta :: HeadingMeta
-  , contents       :: [Block]
-  } deriving (Ord, Eq, Generic)
+data Concept = Concept
+    { getID :: String
+    , getUID :: Integer
+    , getName :: String
+    , getHeadingMeta :: HeadingMeta
+    , getTitle :: [Inline]
+    , getConceptContents :: [Block]
+    } deriving (Ord,Eq,Generic)
 
-instance ToJSON InternalDeckStructLeaf
+instance ToJSON Concept
 
--- instance ToJSON InternalDeckStructLeaf where
--- toJSON s = String $ T.pack $ processLeafContents (contents s)
-type InternalDeckStruct = Tree InternalDeckStructLeaf
+-- instance ToJSON Concept where
+-- toJSON s = String $ T.pack $ processLeafContents (getConceptContents s)
+type ConceptTree = Tree Concept
 
 data InternalDeck = ID
-  { getTitle              :: String
-  , getAuthor             :: String
-  , getCardLevel          :: Integer
-  , getInternalDeckStruct :: InternalDeckStruct
-  , otherMeta             :: String -> Maybe String
-  }
+    { getDeckName :: String
+    , getAuthor :: String
+    , getCardLevel :: Integer
+    , getConceptTree :: ConceptTree
+    , otherMeta :: String -> Maybe String
+    }
 
 instance ToJSON InternalDeck where
-  toJSON d =
-    object
-      [ "title" .= getTitle d
-      , "author" .= getAuthor d
-      , "root" .= toJSON (getInternalDeckStruct d)
-      ]
+    toJSON d =
+        object
+            [ "getTitle" .= getDeckName d
+            , "author" .= getAuthor d
+            , "root" .= toJSON (getConceptTree d)]
 
-getDeckName :: InternalDeck -> String
-getDeckName = getTitle
-
-processLeafContents :: [Block] -> String
-processLeafContents b = writeHtmlString def (Pandoc (Meta Map.empty) b)
+blockToHTML :: [Block] -> String
+blockToHTML b = writeHtmlString def (Pandoc (Meta Map.empty) b)
 
 type Chunk = (Block, [Block])
 
 getLevel :: Chunk -> Int
-getLevel (Header n _ _, _) = n
-getLevel _                 = error "Sorry, chunk is invalid"
+getLevel (Header n _ _,_) = n
+getLevel _ = error "Sorry, chunk is invalid"
 
 add :: Block -> [Chunk] -> [Chunk]
-add x@Header {} cs = cs ++ [(x, [])]
+add x@Header{} cs = cs ++ [(x, [])]
 add x cs@(_:_) =
-  let i = init cs
-      (h, l) = last cs
-  in i ++ [(h, l ++ [x])]
+    let i = init cs
+        (h,l) = last cs
+    in i ++ [(h, l ++ [x])]
 add x []
-    -- This might happen also when some meta info is present but with a blank value
-    -- Pandoc recognizes this as a paragraph and so it appears as some info outside concepts
-    -- final decision; change from:
-    -- error "Sorry, the document should begin with at least one top concept"
-    -- to
- = []
+-- This might happen also when some meta info is present but with a blank value
+-- Pandoc recognizes this as a paragraph and so it appears as some info outside concepts
+-- final decision; change from:
+-- error "Sorry, the document should begin with at least one top concept"
+-- to
+ =
+    []
 
 asDasherized :: [Inline] -> [Char]
 asDasherized = concatMap dasherize
@@ -92,8 +90,8 @@ asPlainString :: [Inline] -> [Char]
 asPlainString = concatMap f
   where
     f (Str s) = s
-    f Space   = " "
-    f _       = "*"
+    f Space = " "
+    f _ = "*"
 
 -- Need to use unfoldr (anamorphism):
 --
@@ -101,75 +99,80 @@ asPlainString = concatMap f
 --
 -- so we need to build:
 --
---    ([Chunk] -> Maybe (InternalDeckStruct, [Chunk]))
+--    ([Chunk] -> Maybe (ConceptTree, [Chunk]))
 --
-toInternalDeckStruct :: Int -> [Chunk] -> Maybe (InternalDeckStruct, [Chunk])
-toInternalDeckStruct n0 ((h@(Header n1 (_, _, mt) text), bs):cs)
+toInternalDeckStruct
+    :: Int -> [Chunk] -> Maybe (ConceptTree, [Chunk])
+toInternalDeckStruct n0 ((h@(Header n1 (_,_,mt) text),bs):cs)
   | n0 == n1 =
-    let getValidBlocks = takeWhile' (\x -> getLevel x > n1)
-        (valid, remaining) = getValidBlocks cs
-        subFor = unfoldr (toInternalDeckStruct $ n0 + 1) valid
-        currentTree =
-          Node
-            (Concept
-               (asDasherized text)
-               0
-               (asPlainString text)
-               (Map.fromList mt)
-               bs)
-            subFor
-    in Just (currentTree, remaining)
+      let getValidBlocks =
+              takeWhile'
+                  (\x ->
+                        getLevel x > n1)
+          (valid,remaining) = getValidBlocks cs
+          subFor = unfoldr (toInternalDeckStruct $ n0 + 1) valid
+          currentTree =
+              Node
+                  (Concept
+                       (asDasherized text)
+                       0
+                       (asPlainString text)
+                       (Map.fromList mt)
+                       text
+                       bs)
+                  subFor
+      in Just (currentTree, remaining)
   | n0 /= n1 = Nothing
 toInternalDeckStruct _ [] = Nothing
 
-parseChunks :: [Chunk] -> InternalDeckStruct
-parseChunks cs =
-  let subFor = unfoldr (toInternalDeckStruct 1) cs
-      currentTree = Node (Concept "root" 0 "Root" Map.empty []) subFor
-  in currentTree
+chunksToConceptTree :: [Chunk] -> ConceptTree
+chunksToConceptTree cs =
+    let subFor = unfoldr (toInternalDeckStruct 1) cs
+        currentTree = Node (Concept "root" 0 "Root" Map.empty [] []) subFor
+    in currentTree
 
 getChunks :: Pandoc -> [Chunk]
 getChunks (Pandoc _ bs) = foldl (flip add) [] bs
 
 parseDeck :: Pandoc -> InternalDeck
 parseDeck x@(Pandoc meta _) =
-  ID
-    (gm "title")
-    (gm "author")
-    (read $ fromMaybe "1" (getMV "level"))
-    (parseChunks $ getChunks x)
-    getMV
+    ID
+        (gm "getTitle")
+        (gm "author")
+        (read $ fromMaybe "1" (getMV "level"))
+        (chunksToConceptTree $ getChunks x)
+        getMV
   where
     gm k = fromMaybe "NA" $ getMV k
     getMV k =
-      case Map.lookup k (unMeta meta) of
-        Just (MetaInlines mi) -> Just $ asPlainString mi
-        _                     -> Nothing
+        case Map.lookup k (unMeta meta) of
+            Just (MetaInlines mi) -> Just $ asPlainString mi
+            _ -> Nothing
 
-singletonTree :: InternalDeckStructLeaf -> InternalDeckStruct
+singletonTree :: Concept -> ConceptTree
 singletonTree a = Node a []
 
-nemptyLeaf :: InternalDeckStructLeaf -> Bool
-nemptyLeaf s = length (contents s) > 0
+nemptyLeaf :: Concept -> Bool
+nemptyLeaf s = length (getConceptContents s) > 0
 
-root xs = Node (Concept "root" 0 "Root" Map.empty []) $ xs
+root xs = Node (Concept "root" 0 "Root" Map.empty [] []) $ xs
 
-deck xs = Node (Concept "deck" 0 "All cards" Map.empty []) $ xs
+deck xs = Node (Concept "deck" 0 "All cards" Map.empty [] []) $ xs
 
-listOfSingletons :: [InternalDeckStructLeaf] -> InternalDeckStruct
+listOfSingletons :: [Concept] -> ConceptTree
 listOfSingletons as = root $ [deck cards]
   where
     cards = map singletonTree $ (filter nemptyLeaf as)
 
-flattenTree :: InternalDeckStruct -> InternalDeckStruct
+flattenTree :: ConceptTree -> ConceptTree
 flattenTree t = listOfSingletons (flatten t)
 
 flattenDeck :: InternalDeck -> InternalDeck
 flattenDeck ideck =
-  ideck
-  { getInternalDeckStruct = (flattenTree $ getInternalDeckStruct ideck)
-  , getCardLevel = 2
-  }
+    ideck
+    { getConceptTree = (flattenTree $ getConceptTree ideck)
+    , getCardLevel = 2
+    }
 
 parseOrg :: String -> InternalDeck
 parseOrg f = parseDeck $ readDoc f
@@ -177,30 +180,30 @@ parseOrg f = parseDeck $ readDoc f
 unLines :: String -> String
 unLines s = intercalate "" $ splitOn "\n" s
 
-renderLeaf :: InternalDeckStructLeaf -> String
-renderLeaf l = unLines (processLeafContents . contents $ l)
+renderLeaf :: Concept -> String
+renderLeaf l = unLines (blockToHTML . getConceptContents $ l)
 
 showDeck :: InternalDeck -> String
-showDeck d = drawTree $ fmap showSL (getInternalDeckStruct d)
+showDeck d = drawTree $ fmap showSL (getConceptTree d)
 
-showSL :: InternalDeckStructLeaf -> String
+showSL :: Concept -> String
 showSL s = getName s ++ ": " ++ renderLeaf s
 
 _drawAsForest :: String -> String
 _drawAsForest f =
-  let d = parseDeck $ readDoc f
-      author = getAuthor d
-      title = getTitle d
-      content = showDeck d
-      l = show (getCardLevel d)
-  in [i|#{title} - #{author} (encoding cards from level #{l} downwards (Root = 0))
+    let d = parseDeck $ readDoc f
+        author = getAuthor d
+        getTitle = getDeckName d
+        content = showDeck d
+        l = show (getCardLevel d)
+    in [i|#{getTitle} - #{author} (encoding cards from level #{l} downwards (Root = 0))
 #{content}
        |]
 
 _printAsJson :: String -> String
 _printAsJson f =
-  let d = parseDeck $ readDoc f
-  in BL.unpack $ encodePretty d
+    let d = parseDeck $ readDoc f
+    in BL.unpack $ encodePretty d
 
 renderAsInternal :: InternalDeck -> IO String
 renderAsInternal x = return $ showDeck x
